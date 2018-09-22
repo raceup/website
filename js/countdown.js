04479ce016855d8a15201814c5f747af018aa14d
jpregno <jacopo.pregnolato@raceup.it>
2018-09-22 12:08:45 +0200

    recruiting page

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Apr 15, 2018 12:00:00").getTime();
+var countDownDate = new Date("Oct 7, 2018 23:59:59").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -19,4 +19,4 @@ var x = setInterval(function() {
     } else {  // stop interval
         clearInterval(x);
     }
-}, 1000);
+}, 1000);  // 1000ms => 1s

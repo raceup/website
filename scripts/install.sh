343ba57938b743ac761cd3566598c035993da41d
sirfoga <sirfoga@protonmail.com>
2019-02-10 11:09:21 +0100

    add package.json; fix WTF #4

--- scripts/install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# coding: utf-8
-
-cd ..  # go back to root
-bundle install --path .gems
--- scripts/run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# coding: utf-8
-
-BUILD_FOLDER="$HOME/.jekyll/build/"
-yes | rm -rf "$BUILD_FOLDER"  # clean builds
-
-cd ..
-bundle exec jekyll serve --watch --profile --trace -d "$BUILD_FOLDER" # run server and profiler
